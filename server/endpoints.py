ad6eedce18e249c3615169d2984c5177ea81ea4d
unknown <bs9107237@gmail.com>
2026-04-13 23:45:55 +0530

    fixed production server startup reload

--- server/endpoints.py
+++ server/endpoints.py
@@ -50,6 +50,3 @@ async def visualize(uploaded_image:UploadFile=File(...)):
     print(f"received image :  {uploaded_image.filename}")
 
     return Response(content=pred,media_type="image/png")
-    
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
